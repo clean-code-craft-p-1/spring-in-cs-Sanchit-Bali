975419f84924e2c43d3b0930e01c5ce27529ec00
Sanchit Mathur <Sanchit.Mathur@philips.com>
2025-07-08 00:13:26 +0530

    TDD

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -8,6 +8,20 @@ namespace Statistics
         public Stats CalculateStatistics(List<float> numbers)
         {
             //Implement statistics here
+            Stats stats = new Stats();
+
+            if (numbers == null || numbers.Count == 0) {
+                stats.average = double.NaN;
+                stats.max = double.NaN;
+                stats.min = double.NaN;
+                return stats;
+            }
+
+            stats.average = numbers.Average();
+            stats.max = numbers.Max();
+            stats.min = numbers.Min();
+
+            return stats;
         }
     }
 }
